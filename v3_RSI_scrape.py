99e391836a054a5531c205a0105b6719499ce679
AnOkayGuardian <38517930+AnOkayGuardian@users.noreply.github.com>
2019-12-07 20:11:45 -0600

    Add code to scrape for images dynamically. Format the href for google sheets. Add .gitignore file and csv for example of returned data.

--- v3_RSI_scrape.py
+++ v3_RSI_scrape.py
@@ -7,15 +7,15 @@ import csv
 
 http = urllib3.PoolManager()
 
-csv_file = open('sc_ship.csv', 'w', newline='', encoding='utf-8')
+csv_file = open('12719_SC_Ships_Available.csv', 'w', newline='', encoding='utf-8')
 csv_writer = csv.writer(csv_file, )
-csv_writer.writerow(['Ship', 'Ship Price', 'Link'])
+csv_writer.writerow(['Ship', 'Ship Price', 'Ship Image', 'Link to Ship'])
 
 shipURLs = requests.get('https://robertsspaceindustries.com/pledge/ships').text
 
 def findShipURL():
     # Note the driver path may be different on your PC
-    browser = webdriver.Chrome(r"C:\Users\kmontgomer\Desktop\chromedriver_win32\chromedriver.exe")
+    browser = webdriver.Chrome(r'C:\Users\User\Desktop\chromedriver_win32 78\chromedriver.exe')
     browser.get("https://robertsspaceindustries.com/pledge/ships")
 
     lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
@@ -52,6 +52,27 @@ for item in generator:
         shipLName = shipName.split('\n')[2]
         shipLName = shipLName.strip()
         data.append(f"""\nShip: {shipFName} {shipLName} \nShip Price: {final_price}""")
-        # print('\n'.join(data))
-        csv_writer.writerow([shipFName + shipLName ,final_price, url])
+        for image in soup.find_all('span', class_='thumbnails clearfix'):
+            findImageURLString = image.findChildren()[2]['href']
+            buildImageURL = f'''=IMAGE("https://robertsspaceindustries.com{findImageURLString}",1)'''
+            data.append(f"""Ship URL: {url} \nImage: {buildImageURL}""")
+            # print('\n'.join(data))
+        csv_writer.writerow([shipFName + shipLName ,final_price, buildImageURL,url])
 csv_file.close()
+
+
+# -------------------- Finding the Description for Each Ship -----------------------
+# source = requests.get('https://robertsspaceindustries.com/pledge/ships/anvil-hornet/F7C-Hornet').text
+# soup = BeautifulSoup(source, 'lxml')
+
+# description = soup.find('div', class_='excerpt')
+# data = []
+# print(f'{description.text.strip()}')
+
+# ----------> Format in Google Sheets for images if I wanted to intergrate pricture of ships???
+# =IMAGE(“https://robertsspaceindustries.com/media/3e0oc9s85451kr/store_slideshow_large/F7c_hornet_front-Right_visual.jpg", 1, 50, 25)
+
+# ----------> Find Image for ships
+# image = soup.find('span', class_='thumbnails clearfix')
+# findATag = image.findChildren()[2]['href']
+# print(findATag)
